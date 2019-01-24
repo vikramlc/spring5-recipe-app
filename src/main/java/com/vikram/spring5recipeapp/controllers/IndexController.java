23d45b4c32fcf733b709d26ca5cd03e26fa8895a
vikramlc <vikram.chawan121@gmail.com>
2019-01-24 15:11:13 +0530

    Completing Assignment.

--- src/main/java/com/vikram/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/vikram/spring5recipeapp/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        System.out.println("Index Controller");
         return "index";
     }
 
