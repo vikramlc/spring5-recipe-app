42c0ebff1e5453b9d7047b1de2b50dacc7a19674
vikramlc <vikram.chawan121@gmail.com>
2019-02-02 14:57:09 +0530

    Mockito Tests

--- src/main/java/com/vikram/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/vikram/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.vikram.spring5recipeapp.services;
 
 import com.vikram.spring5recipeapp.domain.Recipe;
 import com.vikram.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,7 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
-
+        log.debug("I am in the service!!");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
---
+++ src/test/java/com/vikram/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.vikram.spring5recipeapp.services;
+
+import com.vikram.spring5recipeapp.domain.Recipe;
+import com.vikram.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
