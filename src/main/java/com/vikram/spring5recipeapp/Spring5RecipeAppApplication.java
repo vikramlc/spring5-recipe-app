ed2a89a49fb7c55daa2e8fc4393a930db2aaef62
vikramlc <44239429+vikramlc@users.noreply.github.com>
2019-02-02 19:55:29 +0530

    test

--- src/main/java/com/vikram/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/com/vikram/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -2,7 +2,7 @@ package com.vikram.spring5recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
